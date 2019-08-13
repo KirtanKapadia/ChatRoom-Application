6d3f535bc5df858811c83f4cdecba5394bc04cb3
KirtanKapadia <kirtankapadia13@gmail.com>
2019-08-14 01:34:08 +0530

    Client code ready

--- client.py
+++ client.py
@@ -1,14 +1,53 @@
-import zmq
+import socket
+import select
+import errno
+import sys
 
-host = '127.0.0.1'
-port = 6789
-context = zmq.Context()
-client = context.socket(zmq.REQ)
-client.connect("tcp://{}:{}".format(host, port))
-for num in range(1,6):
-	request_str = "message #{}".format(num)
-	request_bytes = request_str.encode('utf-8')
-	client.send(request_bytes)
-	reply_bytes = client.recv()
-	reply_str = reply_bytes.decode('utf-8')
-	print("Sent {}, received {}".format(request_str, reply_str))
+HEADER_LENGTH = 10
+IP = "127.0.0.1"
+PORT = 1235
+
+my_username = input("Username: ")
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP,PORT))
+client_socket.setblocking(False)
+
+username = my_username.encode('utf-8')
+username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
+
+client_socket.send(username_header + username)
+
+while True:
+	message = input(f"{my_username}> ")
+
+	if message:
+		message = message.encode('utf-8')
+		message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+		client_socket.send(message_header + message)
+
+	try:
+		while True:
+			username_header = client_socket.recv(HEADER_LENGTH)
+			if not len(username_header):
+				print("Connection closed by the sever")
+				sys.exit()
+
+			username_length = int(username_header.decode('utf-8').strip())
+			username = client_socket.recv(username_length).decode('utf-8')
+
+			message_header = client_socket.recv(HEADER_LENGTH)
+			message_length = int(message_header.decode('utf-8').strip())
+
+			message = client_socket.recv(message_length).decode('utf-8')
+
+			print(f"{username} > {message}")
+
+
+	except IOError as e:
+		if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+			print("Reading error", str(e))
+			sys.exit()
+		continue	
+
+	except Exception as e:
+		print(f"Error Message:{str(e)}")
